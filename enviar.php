4e99f843fad9f347e5dfb8af92bafda5ccb1a299
website <39255652+brandeen@users.noreply.github.com>
2019-01-27 19:18:39 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,22 @@
+<?php  
+
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "comentariobullon@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:https://rawcdn.githack.com/brandeen/FRBF/39abe6d2c24059ae07ff45426884891dd902fa48/mensaje-de-envio.html');
+
+?>
